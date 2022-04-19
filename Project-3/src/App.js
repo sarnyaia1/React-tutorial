c1881209470ef0c48c0f5272ca39b38166b7fb30
Sarnyai Akos <sarnyaia1@gmail.com>
2022-04-19 11:49:30 +0200

    Create new Components

--- Project-3/src/App.js
+++ Project-3/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import MyForm from '../src/components/MyForm';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>1. This is the MyForm Component</h1>
+        <p>useState(), onChange(), onSubmit()</p>
+        <MyForm />
+      </div>
     </div>
   );
 }
